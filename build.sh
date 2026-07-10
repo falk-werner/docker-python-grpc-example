82487c6bfbcb714d40fa6e09f72efac5c152c737
Falk Werner <falk.werner@gmx.net>
2026-07-10 20:55:24 +0200

    add build workflow
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

--- build.sh
+++ build.sh
@@ -5,5 +5,9 @@ set -e
 PLATFORM=linux/arm64
 
 mkdir -p out
-docker build "--platform=$PLATFORM" --tag python-grpc-greeter -f Dockerfile app
-docker save "--platform=$PLATFORM" -o out/python-grpc-greeter.tar python-grpc-greeter
+docker build \
+    "--platform=$PLATFORM" \
+    --tag python-grpc-greeter \
+    --output type=docker,dest=out/python-grpc-greeter.tar \
+    .
+
